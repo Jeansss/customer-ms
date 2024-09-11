44f34ffb82903563b0bdbc5dad200beb81d20ca4
Jean Silva <jeans_bs@outlook.com>
2024-09-11 01:26:10 -0300

    refactor: change string status to enum

--- src/use-cases/customer/customer.use-case.ts
+++ src/use-cases/customer/customer.use-case.ts
@@ -5,6 +5,7 @@ import { CustomerDTO } from "src/dto/customer.dto";
 import { Customer } from "src/frameworks/data-services/mysql/entities/customer.model";
 import { CustomerRepositoryImpl } from "src/frameworks/data-services/mysql/gateways/customer.repository";
 import { CustomerInfoDTO } from "src/dto/customer-info.dto";
+import { CustomerStatus } from "src/dto/customer-status.enum";
 
 @Injectable()
 export class CustomerUseCases {
@@ -17,7 +18,7 @@ export class CustomerUseCases {
 
     async getCustomerById(id: string): Promise<Customer> {
         const foundCustomer = await this.dataServices.customers.get(id);
-        if (foundCustomer != null) {
+        if (foundCustomer != null && foundCustomer.status === CustomerStatus.ACTIVE) {
             return foundCustomer;
         } else {
             throw new NotFoundException(`Customer with id: ${id} not found at database.`);
@@ -26,8 +27,7 @@ export class CustomerUseCases {
 
     async getCustomerByCPF(customerCPF: string) {
         const foundCustomer = await this.dataServices.customers.getCustomerByCPF(customerCPF);
-
-        if (foundCustomer != null) {
+        if (foundCustomer != null && foundCustomer[0].status === CustomerStatus.ACTIVE) {
             return foundCustomer;
         } else {
             throw new NotFoundException(`Customer with id: ${customerCPF} not found at database.`);
@@ -36,7 +36,7 @@ export class CustomerUseCases {
 
     async createCustomer(customerDTO: CustomerDTO): Promise<Customer> {
         const newCustomer = this.customerFactoryService.createNewCustomer(customerDTO);
-        newCustomer.status = 'active';
+        newCustomer.status = CustomerStatus.ACTIVE;
         return this.dataServices.customers.create(newCustomer);
     }
 
@@ -52,7 +52,7 @@ export class CustomerUseCases {
 
     async inactivateCustomer(customerId: string, customerInfo: CustomerInfoDTO): Promise<Customer> {
         const foundCustomer = await this.getCustomerById(customerId);
-        foundCustomer.status = 'inactive';
+        foundCustomer.status = CustomerStatus.INACTIVE;
         return this.dataServices.customers.update(customerId, foundCustomer);
     }
 
